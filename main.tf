12887b3f3904a2e070997651bae141bf8eba6ffe
ali <syeda7219@gmail.com>
2025-10-22 21:14:20 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbuckettoday34333" {
+  bucket = "terraformbuckettoday34333"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettoday34333"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
